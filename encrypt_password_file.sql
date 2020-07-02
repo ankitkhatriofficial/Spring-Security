abfcf75e7ee82a39da180fbfd7b2bcfbf7a38a78
Ankit Khatri <43426821+Khatri09@users.noreply.github.com>
2020-07-02 14:36:42 +0530

    Create encrypt_password_file.sql

---
+++ encrypt_password_file.sql
@@ -0,0 +1,57 @@
+DROP DATABASE  IF EXISTS `spring_security_demo_bcrypt`;
+
+CREATE DATABASE  IF NOT EXISTS `spring_security_demo_bcrypt`;
+USE `spring_security_demo_bcrypt`;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE `users` (
+  `username` varchar(50) NOT NULL,
+  `password` char(68) NOT NULL,
+  `enabled` tinyint(1) NOT NULL,
+  PRIMARY KEY (`username`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `users`
+--
+-- NOTE: The passwords are encrypted using BCrypt
+--
+-- A generation tool is avail at: http://www.luv2code.com/generate-bcrypt-password
+--
+-- Default passwords here are: fun123
+--
+
+INSERT INTO `users` 
+VALUES 
+('bobby','{bcrypt}$2a$10$HcTS9CBBgk/mpZGop2r6zua5HkAaQUzeoGkFIhdvulgCvGzVUkIzy',1),
+('ankit','{bcrypt}$2a$10$HcTS9CBBgk/mpZGop2r6zua5HkAaQUzeoGkFIhdvulgCvGzVUkIzy',1),
+('khatri','{bcrypt}$2a$10$HcTS9CBBgk/mpZGop2r6zua5HkAaQUzeoGkFIhdvulgCvGzVUkIzy',1);
+
+--
+-- Table structure for table `authorities`
+--
+
+DROP TABLE IF EXISTS `authorities`;
+CREATE TABLE `authorities` (
+  `username` varchar(50) NOT NULL,
+  `authority` varchar(50) NOT NULL,
+  UNIQUE KEY `authorities_idx_1` (`username`,`authority`),
+  CONSTRAINT `authorities_ibfk_1` FOREIGN KEY (`username`) REFERENCES `users` (`username`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `authorities`
+--
+
+INSERT INTO `authorities` 
+VALUES 
+('bobby','ROLE_EMPLOYEE'),
+('ankit','ROLE_EMPLOYEE'),
+('ankit','ROLE_MANAGER'),
+('khatri','ROLE_EMPLOYEE'),
+('khatri','ROLE_MANAGER'),
+('khatri','ROLE_ADMIN');
